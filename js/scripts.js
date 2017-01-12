f8cd4d4db1a776f108cc654090e4fb9c57224ea5
Ian Cabigon <ian@users-MacBook-Pro.local>
2017-01-12 17:04:11 +0800

    added computation

--- js/scripts.js
+++ js/scripts.js
@@ -91,6 +91,54 @@ $(document).ready(function() {
 		  columnWidth: 0,
 		  itemSelector: '.masonry li'
 		});
-		
+	
+	// COMPUTATION MODAL
+	$('#calculateBtn').click(function(){
+		var dutiableValueUSD = parseFloat($('#inActualVal').val()) + parseFloat($('#inFreightCost').val()) + parseFloat($('#inInsuranceCost').val());
+		var xRate = 50;
+		var dutiableValuePeso = dutiableValueUSD * xRate;
+		var rateOfDuty = 0.1; //10%
+		var customsDuty = dutiableValuePeso * rateOfDuty;
+		var brokersFee = dutiableValuePeso * .05;
+		var docStamps = dutiableValuePeso * 0.16;
+		var exciseTax = dutiableValuePeso * .2;
+		var importProcessingFee = dutiableValuePeso > 700000 ? 1000 : 750;
+		var landedCost = dutiableValuePeso + customsDuty + brokersFee + docStamps + importProcessingFee + exciseTax;
+		var vatRate = .12;
+		var vat = landedCost * vatRate;
+		var totalTaxesAndDuties = customsDuty + vat + exciseTax + importProcessingFee;
+		var dutyHandlingFee = totalTaxesAndDuties * .035;
+		var informalEntryDec = totalTaxesAndDuties * .04;
+		var storageFee = landedCost * .02;
+		var collectorsClearance = 500;
+		var totalFees = parseFloat(totalTaxesAndDuties + dutyHandlingFee + informalEntryDec + storageFee).formatMoney(2, '.', ',');
+
+		$('#customsDuty').text(customsDuty.formatMoney(2, '.', ','));
+		$('#vat').text(vat.formatMoney(2, '.', ','));
+		$('#exciseTax').text(exciseTax.formatMoney(2, '.', ','));
+		$('#importProcessingFee').text(importProcessingFee.formatMoney(2, '.', ','));
+		$('#totalTaxesAndDuties').text(totalTaxesAndDuties.formatMoney(2, '.', ','));
+		$('#dutyHandlingFee').text(dutyHandlingFee.formatMoney(2, '.', ','));
+		$('#informalEntryDec').text(informalEntryDec)
+		$('#storageFee').text(storageFee.formatMoney(2, '.', ','));
+		$('#collectorsClearance').text(collectorsClearance.formatMoney(2, '.', ','));
+		$('#totalFees').text(totalFees);
+
+		$('#computation').modal();
+	});
 		
 })(jQuery);
+
+Number.prototype.formatMoney = function(c, d, t){
+var n = this, 
+    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
+
+
+
